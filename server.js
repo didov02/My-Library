be5d9972aebe0793b9858fcd7f252eac15b839c4
Dian Vasilev <92683949+didov02@users.noreply.github.com>
2024-12-26 13:50:36 +0200

    Additional changes on server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authorsRoutes');
 const userRouter = require('./routes/usersRoutes');
+const bookRouter = require('./routes/booksRoutes');
+const libraryBookRouter = require('./routes/libraryBooksRoutes');
+const commentsRouter = require('./routes/commentsRoutes');
+const ratingsRouter = require('./routes/ratingsRoutes');
 const app = express();
 
 const port = process.env.PORT || 8081;
@@ -16,6 +20,10 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
 app.use('/users', userRouter);
+app.use('/books', bookRouter);
+app.use('/libraryBooks', libraryBookRouter);
+app.use('/comments', commentsRouter);
+app.use('/ratings', ratingsRouter);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
